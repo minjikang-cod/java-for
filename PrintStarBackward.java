3915cfd34c572b8c43770937c99ebe26159b0595
minjikang-cod <63547522+minjikang-cod@users.noreply.github.com>
2020-04-13 00:57:40 +0900

    Create PrintStarBackward.java

---
+++ PrintStarBackward.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class PrintStarBackward{
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(bfr.readLine());	//the number of test
+        
+        for (int i=1;i<=num;i++) {
+         	for (int j=num-i;j>0;j--) {
+         		bfw.write(" ");
+         	}
+         	for (int j=0;j<i;j++) {
+         		bfw.write("*");
+         	}
+         	bfw.write("\n");
+        }
+        bfw.flush();
+        bfw.close();
+    }
+}
