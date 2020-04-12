abab2f908ccd1bea779a0e0f9abcc3ead72ee3ef
minjikang-cod <63547522+minjikang-cod@users.noreply.github.com>
2020-04-13 00:32:36 +0900

    Create SequencialAddition.java

---
+++ SequencialAddition.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int num = scan.nextInt();	//count for test
+        int sum = 0;				
+        
+        for (int i=0;i<num;i++) {
+        	sum += i+1;
+        }
+        System.out.println(sum);
+        
+        scan.close();
+    }
+}
