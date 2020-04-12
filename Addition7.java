c152f11a7858861832efa1ff6e78a58bfd081bc7
minjikang-cod <63547522+minjikang-cod@users.noreply.github.com>
2020-04-13 00:49:43 +0900

    Create Addition7.java
    If I input the number of test, this code recieves the input A,B for N test and prints the output.

---
+++ Addition7.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Addition7{
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(bfr.readLine());	//the number of test
+        
+        for (int i=0;i<num;i++) {
+        	String str = bfr.readLine();
+        	StringTokenizer st = new StringTokenizer(str);
+        	
+        	int A = Integer.parseInt(st.nextToken());
+        	int B = Integer.parseInt(st.nextToken());
+        	int sum = A + B;
+        	
+        	bfw.write("Case #"+Integer.toString(i+1)+": "+Integer.toString(sum)+"\n");
+        }
+        bfw.flush();
+        bfw.close();
+    }
+}
