ac9f8333f1307be1c608acbbae39bc5059f350ac
minjikang-cod <63547522+minjikang-cod@users.noreply.github.com>
2020-04-13 00:33:57 +0900

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main{
+public class MultiplicationTable{
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
